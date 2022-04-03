729800bc66a5c62d928d722c275b3fc56127b005
zh-hu <zhanghu1026@icloud.com>
2022-04-03 15:23:21 +0800

    Initial commit

---
+++ cloudalibaba-provider-payment9001/src/main/java/com/atguigu/springcloud/alibaba/PaymentMain9001.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author zhanghu
+ * @create 2022-04-03 15:16
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9001.class, args);
+    }
+
+}
